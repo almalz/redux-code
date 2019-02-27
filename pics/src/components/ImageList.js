6597cdacb868d8e598b52b219fdf4bf06be05e22
alexismalzieu <alex.malzieu@gmail.com>
2019-02-27 23:18:24 +0100

    set up grid layout

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,15 +1,14 @@
 import React from 'react';
+import ImageCard from './ImageCard';
+import './ImageList.css'
 
 const ImageList = (props) => {
 
-	const images = props.images.map(({description, id, urls})=>{
-		return <img
-			key={id}
-			alt={description}
-			src={urls.regular}/>
+	const images = props.images.map(image=>{
+		return <ImageCard image={image} key={image.id}/>
 	});
 
-	return <div>{images}</div>
+	return <div className="image-list">{images}</div>
 };
 
 export default ImageList;
